aedde2c02d89e4faee1fc272493ac9312fc19c51
Bahrain67 <facetube67@gmail.com>
2025-06-10 04:22:09 +0300

    Delete templates directory

--- templates/error.php
+++
@@ -1,9 +0,0 @@
-<div class="card fade-in" style="max-width: 500px; margin: 2rem auto;">
-    <h2><i class="fas fa-exclamation-triangle"></i> خطأ</h2>
-    <p style="text-align: center; color: #dc2626;">
-        الصفحة غير موجودة أو غير مصرح لك بالوصول إليها!
-    </p>
-    <a href="index.php?action=home" class="btn btn-primary" style="width: 100%; margin-top: 1rem;">
-        <i class="fas fa-home"></i> العودة إلى الرئيسية
-    </a>
-</div>
